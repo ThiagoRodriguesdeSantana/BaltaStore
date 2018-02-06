cea0fbea7df9c14ec8abcb5776ea257d1a0d74d5
Thiago Rodrigues <thiagorodriguescamara@gmail.com>
2018-02-05 22:56:45 -0200

    Criando Commands

---
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommand/Input/CreateCustomerCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommand.Input
+{
+    class CreateCustomerCommand
+    {
+    }
+}
